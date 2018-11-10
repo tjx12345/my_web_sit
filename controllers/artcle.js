a2fbaae3e71a69e003713354752ba5234f128e2e
tjx12345 <18627888722@163.com>
2018-11-10 16:38:25 +0800

    修复路径问题

--- controllers/artcle.js
+++ controllers/artcle.js
@@ -1,29 +1,28 @@
 let a = exports;
 const fs = require('fs');
 const path = require('path');
-function getDir({dir,filter}){
-    let files = fs.readdirSync(dir);
+function getDir({dir='',filter,ctx}){
+    let files = fs.readdirSync(path.join(ctx.config.resourceDir,dir));
     return files.map(f=>{
         if (filter === undefined)return {title:f};
         let i = -1;
         if((i = f.indexOf(filter)) != -1) {
             let filename = f.substr(0,i);
-            return {title:f.substr(0,i),dir:dir + '/' + f}; 
+            return {title:f.substr(0,i),dir:dir+ '/' + f}; 
         } else {
             return null;
         }
     });
 }
 a.getTitle = async (ctx,next)=>{
-   let files = getDir({dir:path.join(ctx.config.resourceDir,ctx.params.type),filter:'.'});
+   let files = getDir({dir:ctx.params.type,filter:'.',ctx});
 
   await ctx.render('time', {
     dir: files
   })
 }
 a.showIndex = async (ctx,next) => {
-     let categories = getDir({dir:ctx.config.resourceDir});
-     console.log(categories)
+     let categories = getDir({ctx});
      await ctx.render('index',{
         categories
      });
